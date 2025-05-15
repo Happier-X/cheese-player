ee0abaf923e524edcbf587bda99e685c63cbcc8b
Happier <zhf5256@qq.com>
2025-05-15 23:20:18 +0800

    feat: 完成进度条更新功能

--- src/layout/normal/playBar/components/ProgressBar.vue
+++ src/layout/normal/playBar/components/ProgressBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full h-[15px] py-[6px] cursor-pointer">
     <div class="w-full h-[3px] bg-gray-300 relative">
-      <div class="w-2/3 h-full relative">
-        <div class="h-full w-full bg-black transition-all duration-300"></div>
+      <div class="w-0 h-full relative" :style="{ width: playerStore.progress + '%' }">
+        <div class="h-full w-full bg-black"></div>
         <div
           class="w-[12px] h-[12px] rounded-box shadow bg-black absolute right-0 top-1/2 transform -translate-y-1/2 translate-x-1/2"
         ></div>
@@ -10,3 +10,8 @@
     </div>
   </div>
 </template>
+<script setup lang="ts">
+import { usePlayerStore } from '../../../../stores/player';
+
+const playerStore = usePlayerStore();
+</script>
