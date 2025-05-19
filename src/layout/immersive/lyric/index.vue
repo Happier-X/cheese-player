c7db0e6c041d96cfd6ad47c0de191b202d611bda
Happier <zhf5256@qq.com>
2025-05-19 22:49:12 +0800

    feat: 完成部分歌词展示效果

--- src/layout/immersive/lyric/index.vue
+++ src/layout/immersive/lyric/index.vue
@@ -1,5 +1,14 @@
 <template>
-    {{ playerStore.lyricLines[playerStore.currentLyricLine] }}
+    <div class="w-full h-full overflow-x-hidden overflow-y-scroll">
+        <div class="w-full" v-for="(line, index) in playerStore.lyricLines" :key="index">
+            <div class="w-full h-10 text-center text-2xl" :class="{
+                'text-blue-500': index === playerStore.currentLyricLine,
+                'text-gray-500': index !== playerStore.currentLyricLine
+            }">
+                {{ line.text }}
+            </div>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
 import { usePlayerStore } from "../../../stores/player";
