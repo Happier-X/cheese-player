38955a2a1f545850bb1eb8e11b8a5341ba917765
Happier <zhf5256@qq.com>
2025-06-01 11:10:36 +0800

    feat: 增加窗口拖动效果

--- src/layout/normal/components/TitleBar.vue
+++ src/layout/normal/components/TitleBar.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="flex items-center justify-between border-b-1">
-        <div class="px-3">Cheese Player</div>
+    <div
+        data-tauri-drag-region
+        class="flex items-center justify-between border-b-1">
+        <div class="px-3 select-none">Cheese Player</div>
         <WindowControls />
     </div>
 </template>
