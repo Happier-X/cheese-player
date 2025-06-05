d21f85b649dbcdff46caa35e0ffb8d4ea198ca36
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-06-05 11:25:33 +0800

    feat: 完成沉浸式标题栏

--- src/components/layout/Immersive.vue
+++ src/components/layout/Immersive.vue
@@ -1,4 +1,21 @@
 <template>
-    <div class="w-full h-full bg-pink-300"></div>
+    <div class="w-full h-full">
+        <div data-tauri-drag-region class="flex items-center justify-between">
+            <div></div>
+            <div class="flex items-center">
+                <Button variant="ghost" @click="layoutStore.toggleLayout()">
+                    <CloseImmersiveIcon />
+                </Button>
+                <WindowControls />
+            </div>
+        </div>
+    </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WindowControls from '@/components/common/WindowControls.vue'
+import { Button } from '@/components/ui/button'
+import { Minimize2 as CloseImmersiveIcon } from 'lucide-vue-next'
+import { useLayoutStore } from '@/stores/layout'
+
+const layoutStore = useLayoutStore()
+</script>
