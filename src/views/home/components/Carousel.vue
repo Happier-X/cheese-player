444ac218d3150b42c44f24de86d68988fc04c845
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-06-06 13:31:29 +0800

    feat: 完成封面展示

--- src/views/home/components/Carousel.vue
+++ src/views/home/components/Carousel.vue
@@ -1,9 +1,14 @@
 <template>
-    <Card class="w-full h-full">
-        <CardContent>
+    <Card class="w-full h-full p-0">
+        <CardContent class="w-full h-full p-0">
             <Carousel class="w-full h-full [&>div]:h-full!">
                 <CarouselContent class="w-full h-full -ml-0">
-                    <CarouselItem class="pl-0">Card Content</CarouselItem>
+                    <CarouselItem class="w-full h-full pl-0 rounded-lg">
+                        <img
+                            src="https://img.daisyui.com/images/profile/demo/1@94.webp"
+                            class="h-full rounded-lg"
+                            alt="" />
+                    </CarouselItem>
                 </CarouselContent>
             </Carousel>
         </CardContent>
