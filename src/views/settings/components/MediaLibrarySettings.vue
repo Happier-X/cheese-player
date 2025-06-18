d4acad781dc6448cfe62b507b1dedc617606a580
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-06-18 10:55:59 +0800

    feat: 完成媒体库设置

--- src/views/settings/components/MediaLibrarySettings.vue
+++ src/views/settings/components/MediaLibrarySettings.vue
@@ -1,138 +1,94 @@
 <template>
-    <!-- <div class="flex flex-col gap-4">
-        <div class="text-lg font-bold border-l-[var(--foreground)] border-l-3 pl-3">
-            媒体库设置
-        </div>
-        <Dialog>
-            <Card class="flex-row">
-                <CardHeader class="flex-1">
-                    <CardTitle as-child>媒体服务</CardTitle>
-                    <CardDescription class="flex items-center gap-1">
-                        <component :is="isLinked ? LinkIcon : UnlinkIcon" :size="14" />
-                        {{ isLinked ? '已连接' : '未连接' }}
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <DialogTrigger as-child>
-                        <Button size="icon" variant="ghost" @click="handleOpenMediaServerDialog">
-                            <EditIcon />
-                        </Button>
-                    </DialogTrigger>
-                </CardContent>
-            </Card>
-            <DialogContent description="配置媒体服务器的连接信息，包括服务器地址、用户名和密码">
-                <DialogHeader>
-                    <DialogTitle>媒体服务</DialogTitle>
-                    <DialogDescription>
-                        请填写您的媒体服务器连接信息
-                    </DialogDescription>
-                </DialogHeader>
-                <form id="mediaServerForm" autocomplete="off" @submit="handleMediaServerFormSubmit"
-                    class="flex flex-col gap-4">
-                    <FormField v-slot="{ componentField }" name="url">
-                        <FormItem>
-                            <FormLabel>服务地址</FormLabel>
-                            <FormControl>
-                                <Input type="text" placeholder="请输入服务地址" v-bind="componentField" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
-                    <FormField v-slot="{ componentField }" name="username">
-                        <FormItem>
-                            <FormLabel>用户名</FormLabel>
-                            <FormControl>
-                                <Input type="text" placeholder="请输入用户名" v-bind="componentField" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
-                    <FormField v-slot="{ componentField }" name="password">
-                        <FormItem>
-                            <FormLabel>密码</FormLabel>
-                            <FormControl>
-                                <Input type="password" placeholder="请输入密码" v-bind="componentField" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
-                </form>
-                <DialogFooter>
-                    <DialogClose as-child>
-                        <Button variant="outline">取消</Button>
-                    </DialogClose>
-                    <DialogClose as-child>
-                        <Button type="submit" form="mediaServerForm">
-                            保存
-                        </Button>
-                    </DialogClose>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    </div> -->
-    <n-list hoverable clickable bordered>
-        <n-list-item>媒体服务</n-list-item>
-        <n-list-item>
-            <n-thing title="他在时间门外" content-style="margin-top: 10px;">
-                <template #description>
-                    <n-space size="small" style="margin-top: 4px">
-                        <n-tag :bordered="false" type="info" size="small">
-                            环形公路
-                        </n-tag>
-                        <n-tag :bordered="false" type="info" size="small">
-                            潜水艇司机
-                        </n-tag>
-                    </n-space>
-                </template>
-                最新的打印机
-                <br />
-                复制着彩色傀儡
-                <br />
-                早上好我的罐头先生
-                <br />
-                让他带你去被工厂敲击
-            </n-thing>
-        </n-list-item>
-    </n-list>
+    <div class="pt-5">
+        <NH3 prefix="bar">媒体库设置</NH3>
+        <NFlex vertical :size="12">
+            <NCard>
+                <NFlex justify="space-between" align="center">
+                    <NFlex vertical>
+                        <NText strong class="font-size-4">服务地址</NText>
+                        <NTag
+                            :type="
+                                isLinked === 'false'
+                                    ? 'error'
+                                    : isLinked === 'true'
+                                      ? 'success'
+                                      : 'warning'
+                            "
+                            round
+                            :bordered="false"
+                            size="small">
+                            {{
+                                isLinked === 'false'
+                                    ? '未连接'
+                                    : isLinked === 'true'
+                                      ? '已连接'
+                                      : '连接中'
+                            }}
+                            <template #icon>
+                                <component
+                                    :is="
+                                        isLinked === 'false'
+                                            ? UnlinkIcon
+                                            : isLinked === 'true'
+                                              ? LinkIcon
+                                              : PingingIcon
+                                    "
+                                    :size="14"
+                                    :class="
+                                        isLinked === 'pinging'
+                                            ? 'animate-spin'
+                                            : ''
+                                    " />
+                            </template>
+                        </NTag>
+                    </NFlex>
+                    <NInput
+                        v-model:value="mediaServer.url"
+                        type="text"
+                        placeholder="请输入服务地址"
+                        :on-blur="handleMediaServerFormSubmit"
+                        class="w-60!" />
+                </NFlex>
+            </NCard>
+            <NCard>
+                <NFlex justify="space-between" align="center">
+                    <NFlex vertical>
+                        <NText strong class="font-size-4">用户名</NText>
+                    </NFlex>
+                    <NInput
+                        v-model:value="mediaServer.username"
+                        type="text"
+                        placeholder="请输入用户名"
+                        :on-blur="handleMediaServerFormSubmit"
+                        class="w-60!" />
+                </NFlex>
+            </NCard>
+            <NCard>
+                <NFlex justify="space-between" align="center">
+                    <NFlex vertical>
+                        <NText strong class="font-size-4">密码</NText>
+                    </NFlex>
+                    <NInput
+                        v-model:value="mediaServer.password"
+                        type="password"
+                        show-password-on="click"
+                        placeholder="请输入密码"
+                        :on-blur="handleMediaServerFormSubmit"
+                        class="w-60!" />
+                </NFlex>
+            </NCard>
+        </NFlex>
+    </div>
 </template>
 <script setup lang="ts">
 import subsonicApi from '@/api/subsonic'
-import { Button } from '@/components/ui/button'
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle
-} from '@/components/ui/card'
-import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger
-} from '@/components/ui/dialog'
-import {
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
 import { Store } from '@tauri-apps/plugin-store'
-import { toTypedSchema } from '@vee-validate/zod'
 import {
-    SquarePen as EditIcon,
+    Loader as PingingIcon,
     CircleCheck as LinkIcon,
     CircleX as UnlinkIcon
 } from 'lucide-vue-next'
-import { useForm } from 'vee-validate'
 import { onMounted, ref } from 'vue'
-import * as z from 'zod'
 
 const mediaServer = ref({
     url: '',
@@ -141,45 +97,30 @@ const mediaServer = ref({
 })
 let settingsStore = null as Store | null
 onMounted(async () => {
-    handlePing()
-})
-const isLinked = ref(false)
-const handlePing = async () => {
-    try {
-        const res: any = await subsonicApi.ping()
-        if (res.status === 'ok') {
-            isLinked.value = true
-        } else {
-            isLinked.value = false
-        }
-    } catch (error) {
-        isLinked.value = false
-    }
-}
-const handleOpenMediaServerDialog = async () => {
     settingsStore = await Store.load('store.settings')
     mediaServer.value = (await settingsStore?.get('mediaServer')) || {
         url: '',
         username: '',
         password: ''
     }
-    mediaServerForm.setValues(mediaServer.value)
-}
-const mediaServerFormSchema = toTypedSchema(
-    z.object({
-        url: z.string().min(1, '服务地址不能为空').url('请输入合法的URL'),
-        username: z.string().optional(),
-        password: z.string().optional()
-    })
-)
-const mediaServerForm = useForm({
-    validationSchema: mediaServerFormSchema,
-    initialValues: mediaServer.value
+    handlePing()
 })
-const handleMediaServerFormSubmit = mediaServerForm.handleSubmit(
-    async (values) => {
-        await settingsStore?.set('mediaServer', values)
-        handlePing()
+const isLinked = ref('false')
+const handlePing = async () => {
+    isLinked.value = 'pinging'
+    try {
+        const res: any = await subsonicApi.ping()
+        if (res.status === 'ok') {
+            isLinked.value = 'true'
+        } else {
+            isLinked.value = 'false'
+        }
+    } catch (error) {
+        isLinked.value = 'false'
     }
-)
+}
+const handleMediaServerFormSubmit = async () => {
+    await settingsStore?.set('mediaServer', mediaServer.value)
+    handlePing()
+}
 </script>
