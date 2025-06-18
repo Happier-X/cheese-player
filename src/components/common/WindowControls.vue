d42af2809e50d1edce7634139be2a1cb2501795e
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-06-18 17:55:13 +0800

    style: 调整窗口控制样式

--- src/components/common/WindowControls.vue
+++ src/components/common/WindowControls.vue
@@ -1,17 +1,21 @@
 <template>
-    <NButton text :focusable="false" @click="handleMinimize">
-        <MinimizeIcon :size="16" />
-    </NButton>
-    <NButton
-        text
-        :focusable="false"
-        class="scale-x-[-1]"
-        @click="handleToggleScreenSize">
-        <component :is="isMaximized ? RestoreIcon : MaximizeIcon" :size="16" />
-    </NButton>
-    <NButton class="mr-3" text :focusable="false" @click="handleClose">
-        <CloseIcon :size="16" />
-    </NButton>
+    <NFlex class="pr-3" :wrap="false">
+        <NButton text :focusable="false" @click="handleMinimize">
+            <MinimizeIcon :size="16" />
+        </NButton>
+        <NButton
+            text
+            :focusable="false"
+            class="scale-x-[-1]"
+            @click="handleToggleScreenSize">
+            <component
+                :is="isMaximized ? RestoreIcon : MaximizeIcon"
+                :size="16" />
+        </NButton>
+        <NButton text :focusable="false" @click="handleClose">
+            <CloseIcon :size="16" />
+        </NButton>
+    </NFlex>
 </template>
 <script setup lang="ts">
 import {
