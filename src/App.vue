43ae6cd9b08206acefc31e57446a8a9092c7c905
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-06-18 16:22:58 +0800

    feat: 增加播放记忆功能

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,22 @@
 </template>
 <script setup lang="ts">
 import Layout from '@/components/layout/index.vue'
+import { getCurrentWindow } from '@tauri-apps/api/window'
+import { usePlayerStore } from '@/stores/player'
+import { onMounted } from 'vue'
+import { Store } from '@tauri-apps/plugin-store'
+
+const playerStore = usePlayerStore()
+const appWindow = getCurrentWindow()
+appWindow.onCloseRequested(async () => {
+    await playerStore.saveCurrentPlayInfo()
+    await appWindow.close()
+})
+let store = null as Store | null
+onMounted(async () => {
+    store = await Store.load('store.player')
+    const res = (await store?.get('currentPlayInfo')) as any
+    await playerStore.loadSong(res.currentSongInfo)
+    playerStore.setPlayQueue(res.currentPlayQueue)
+})
 </script>
